b018482c275dfce4dcd2dca0dab8b2bffcda88fc
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2023-01-01 19:08:09 +0530

    Create wordPattern.java

---
+++ Easy Problems (Naam ka easy)/wordPattern.java
@@ -0,0 +1,27 @@
+/*Q: https://leetcode.com/problems/word-pattern
+
+sollution class*/
+
+class Solution {
+public boolean wordPattern(String pattern, String s) {
+Map<Character, String> map = new HashMap<>();
+String[] words = s.split(" ");
+if (pattern.length() != words.length) {
+return false;
+}
+for (int i = 0; i < pattern.length(); i++) {
+char c = pattern.charAt(i);
+if (map.containsKey(c)) {
+if (!map.get(c).equals(words[i])) {
+return false;
+}
+} else {
+if (map.containsValue(words[i])) {
+return false;
+}
+map.put(c, words[i]);
+}
+}
+return true;
+}
+}
